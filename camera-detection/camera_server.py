e2b767fc3ce03ba74168ab9ba78024b296e37d40
secondmailaltt <secondmailaltt@gmail.com>
2025-06-30 22:09:23 +0500

    Update camera_server.py

--- camera-detection/camera_server.py
+++ camera-detection/camera_server.py
@@ -280,4 +280,4 @@ def release_camera():
     return 'OK', 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001, threaded=True)
+    app.run(host='0.0.0.0', port=3000, threaded=True)
